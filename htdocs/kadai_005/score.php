ab4bc86bbe24e9014e3de1b51e35fd12ce0fec26
erimama12 <e.r.8i222na@gmail.com>
2025-11-10 13:45:20 +0900

    kadai_005をアップロード

---
+++ htdocs/kadai_005/score.php
@@ -0,0 +1,21 @@
+<?php
+$score1 = 80;
+$score2 = 50;
+$score3 = 55;
+$score4 = 40;
+$score5 = 25;
+$score6 = 25;
+$score7 = 80;
+$score8 = 90;
+$score9 = 35;
+$score10 = 60;
+
+// 合計点を計算
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 平均点を計算
+$score_average = $total_score / 10;
+
+// 平均点を出力
+echo $score_average;
+?>
