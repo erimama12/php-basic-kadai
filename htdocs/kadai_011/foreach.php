86c506d002bb13a17607efea6046209a6718816f
erimama12 <e.r.8i222na@gmail.com>
2025-11-10 15:18:11 +0900

    kadai_011をアップロード

---
+++ htdocs/kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<?php
+$personal_data = [
+    "name" => "玉ねぎ",
+    "price" => "200",
+    "location" => "北海道"
+];
+
+// キーを日本語でマッピング
+$key_mapping = [
+    "name" => "名前",
+    "price" => "値段",
+    "location" => "産地"
+];
+
+foreach ($personal_data as $key => $value) {
+    // ": "のフォーマットで出力
+    echo "{$key_mapping[$key]}: {$value}<br>";
+}
+?>
