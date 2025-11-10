27bf5197e5121cfe4f3943dff18ca6bfc74323ab
erimama12 <e.r.8i222na@gmail.com>
2025-11-10 17:00:31 +0900

    kadai_

---
+++ htdocs/kadai_sort/sort.php
@@ -0,0 +1,26 @@
+<?php
+function sort_2way(array &$array, bool $order)
+{
+    if ($order === true) {
+        sort($array); // 昇順
+    } else {
+        rsort($array); // 降順
+    }
+}
+
+$nums = [15, 4, 18, 23, 10];
+
+// 昇順
+sort_2way($nums, true);
+echo "昇順にソートします。<br>";
+foreach ($nums as $num) {
+    echo $num . "<br>";
+}
+
+// 降順
+sort_2way($nums, false);
+echo "降順にソートします。<br>";
+foreach ($nums as $num) {
+    echo $num . "<br>";
+}
+?>
